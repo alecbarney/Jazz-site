4c50e02e5b6355daf8efa301c73507dda3e9d791
Alec Barney <alecbarney34@gmail.com>
2022-04-15 10:28:02 -0600

    added background test video

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import './App.css';
 import Navbar from "./components/Navbar"
 import Recent from "./components/Recent"
+import Video from "./video/Jazz-video.mp4"
 
 
 function App() {
+
+
+
   return (
     <div className="App">
+        <video loop autoPlay muted
+        style={{
+          position: "absolute",
+          width: "100%",
+          left: "50%",
+          top: "50%",
+          height: "100%",
+          objectFit: "cover",
+          transform: "translate(-50%, -50%)",
+          zIndex: "-1"
+        }}
+        >
+        <source
+          src= {Video}
+          type="video/mp4"
+        />
+        Your browser does not support the video tag.
+      </video>
       <Navbar />
       <Recent />
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,13 @@
 import React from "react"
-const axios = require("axios");
+import Jazzlogo from "./images/jazz-logo.png"
 
 
 
 export default function Navbar (){
     return(
         <nav className="navBar">
-        <div className="title--navBar">JAZZ NATION</div>
+        {/* <div className="title--navBar">JAZZ NATION</div> */}
+        <img className ="title--navBar" src={Jazzlogo} alt="Jazzlogo"/>
         </nav>
     )
 }
